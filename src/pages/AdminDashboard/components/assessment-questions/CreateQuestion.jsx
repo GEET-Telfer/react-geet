b4aafbce1b8d9dbf7528226ea79b85417a5d3869
zwang1997 <zwang684@uottawa.ca>
2022-12-01 08:15:17 -0500

    fix bug on inconsistent create-question form values

--- src/pages/AdminDashboard/components/assessment-questions/CreateQuestion.jsx
+++ src/pages/AdminDashboard/components/assessment-questions/CreateQuestion.jsx
@@ -1,120 +1,136 @@
-import { useRef, useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import RangeSlider from 'react-bootstrap-range-slider';
-import Toast from 'react-bootstrap/Toast';
-import ToastContainer from 'react-bootstrap/ToastContainer';
+import { useRef, useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import RangeSlider from "react-bootstrap-range-slider";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
 
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css'; // or include from a CDN
-import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css"; // or include from a CDN
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
 
 export default function CreateQuestion(props) {
+  // State
+  const [scoring, setScoring] = useState(5);
+  const [show, setShow] = useState(false);
+  const [component, setComponent] = useState(
+    "Commitment to Equity, Diversity and Inclusion"
+  );
+  const descriptionRef = useRef("");
+  const [hasNA, setHasNA] = useState(false);
 
-    // State
-    const [scoring, setScoring] = useState(5);
-    const [show, setShow] = useState(false);
-
-    // Ref
-    const componentRef = useRef("Commitment to Equity, Diversity and Inclusion");
-    const descriptionRef = useRef("");
-    const hasNARef = useRef(false);
-
-    const handleCreateQuestion = (async () => {
-        const data = {
-            component: componentRef.current,
-            description: descriptionRef.current,
-            scoring: scoring,
-            hasNA: hasNARef.current.value === 'on' ? 1 : 0
-        };
-
-        await axios.post(`${process.env.REACT_APP_GATEWAY_ENDPOINT}/admin/assessment/create`, data)
-            .then(() => {
-                setShow(true);
-                componentRef.current = "Commitment to Equity, Diversity and Inclusion";
-                descriptionRef.current = "";
-                hasNARef.current = false;
-                setScoring(5);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    });
-
-    // componentRef.current = event.target.value
-    return (
-        <div>
-            <Row>
-                <ToastContainer className="p-3" position={'middle-center'}>
-                    <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
-                        <Toast.Body>Assessment Question Is Added</Toast.Body>
-                    </Toast>
-                </ToastContainer>
-            </Row>
-            <Row>
-                <Form>
-                    <Form.Group as={Row} className="mb-3">
-                        <Col xs={12}>
-                            <Form.Label>Question Component</Form.Label>
-                            <Form.Select
-                                onChange={(event) => componentRef.current = event.target.value}>
-                                <option value="Commitment to Equity, Diversity and Inclusion">Commitment to Equity, Diversity and Inclusion</option>
-                                <option value="Gender Expertise">Gender Expertise</option>
-                                <option value="Access to Resources">Access to Resources</option>
-                                <option value="Program Design">Program Design</option>
-                                <option value="Program Development">Program Development</option>
-                                <option value="Program Delivery">Program Delivery</option>
-                                <option value="Program Evaluation">Program Evaluation</option>
-                            </Form.Select>
-                        </Col>
-                    </Form.Group>
+  const handleCreateQuestion = async () => {
+    const data = {
+      component: component,
+      description: descriptionRef.current,
+      scoring: scoring,
+      hasNA: hasNA,
+    };
 
 
-                    <Form.Group
-                        as={Row} className="mb-3"
-                        value={descriptionRef}
-                        onChange={(event) => descriptionRef.current = event.target.value}>
-                        <Col>
-                            <Form.Label>Question Description</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
-                        </Col>
-                    </Form.Group>
+    await axios
+      .post(
+        `${process.env.REACT_APP_GATEWAY_ENDPOINT}/admin/assessment/create`,
+        data
+      )
+      .then(() => {
+        setShow(true);
+        document.getElementById("create-question-form").reset();
+        setComponent("Commitment to Equity, Diversity and Inclusion");
+        setScoring(5);
+        setHasNA(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-                    <Form.Group as={Row} className="mb-3">
-                        <Col xs="12">
-                            <Form.Label> Scoring </Form.Label>
-                            <RangeSlider
-                                value={scoring}
-                                onChange={e => setScoring(e.target.value)}
-                                step={2}
-                                min={1}
-                                max={11}
-                                tooltip={"on"}
-                                tooltipPlacement={"top"}
-                            />
-                        </Col>
-                    </Form.Group>
+  // componentRef.current = event.target.value
+  return (
+    <div>
+      <Row>
+        <ToastContainer className="p-3" position={"middle-center"}>
+          <Toast
+            onClose={() => setShow(false)}
+            show={show}
+            delay={3000}
+            autohide
+          >
+            <Toast.Body>Assessment Question Is Added</Toast.Body>
+          </Toast>
+        </ToastContainer>
+      </Row>
+      <Row>
+        <Form id="create-question-form">
+          <Form.Group as={Row} className="mb-3">
+            <Col xs={12}>
+              <Form.Label>Question Component</Form.Label>
+              <Form.Control
+                as={"select"}
+                onChange={(event) => {
+                  setComponent(event.target.value);
+                }}
+              >
+                <option value="Commitment to Equity, Diversity and Inclusion">
+                  Commitment to Equity, Diversity and Inclusion
+                </option>
+                <option value="Gender Expertise">Gender Expertise</option>
+                <option value="Access to Resources">Access to Resources</option>
+                <option value="Program Design">Program Design</option>
+                <option value="Program Development">Program Development</option>
+                <option value="Program Delivery">Program Delivery</option>
+                <option value="Program Evaluation">Program Evaluation</option>
+              </Form.Control>
+            </Col>
+          </Form.Group>
 
-                    <Form.Group as={Row} className="mb-3">
-                        <Col xs={12} >
-                            <Form.Check
-                                type="switch"
-                                id="switch-has-na"
-                                label="Has N/A"
-                                ref={hasNARef}
-                            />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Col xs={12}>
-                            <Button variant="primary" onClick={handleCreateQuestion} >
-                                Submit
-                            </Button>
-                        </Col>
-                    </Form.Group>
-                </Form>
-            </Row >
-        </div>
-    )
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            value={descriptionRef}
+            onChange={(event) => (descriptionRef.current = event.target.value)}
+          >
+            <Col>
+              <Form.Label>Question Description</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col xs="12">
+              <Form.Label> Scoring </Form.Label>
+              <RangeSlider
+                value={scoring}
+                onChange={(e) => setScoring(e.target.value)}
+                step={2}
+                min={1}
+                max={11}
+                tooltip={"on"}
+                tooltipPlacement={"top"}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col xs={12}>
+              <Form.Switch
+                onChange={(event) => setHasNA(!hasNA)}
+                checked={hasNA}
+                id="switch-has-na"
+                label="Has N/A"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Col xs={12}>
+              <Button variant="primary" onClick={handleCreateQuestion}>
+                Submit
+              </Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </Row>
+    </div>
+  );
 }
