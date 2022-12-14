b8e9afc4eb98e92866abf96ec027aba8acdf5b41
zwang1997 <zwang684@uottawa.ca>
2022-12-13 22:28:19 -0500

    attempt to fix issue with user response export

--- src/pages/AdminDashboard/components/user-responses/ViewUserResponse.jsx
+++ src/pages/AdminDashboard/components/user-responses/ViewUserResponse.jsx
@@ -9,32 +9,28 @@ import exportFromJSON from 'export-from-json'
 export default function ViewUserResponse() {
   const [userResponse, setUserResponse] = useState();
   const [metaQuestion, setMetaQuestion] = useState();
-  useEffect(() => {
-    const fetchAllUserResponse = axios.get(
-      `${process.env.REACT_APP_HOST}/wp-json/user-response/v1/find-all`
-    );
-    const fetchMetaQuestion = axios.get(
-      `${process.env.REACT_APP_HOST}/wp-json/assessment/v1/find-all`
-    );
 
-    axios
-      .all([fetchAllUserResponse, fetchMetaQuestion])
-      .then(
-        axios.spread((resUserResponse, resMetaQuestion) => {
-          setUserResponse(resUserResponse.data.data);
-          setMetaQuestion(resMetaQuestion.data.data);
-        })
-      )
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+  const fetchAllUserResponse = async() => await axios.get(
+    `${process.env.REACT_APP_HOST}/wp-json/user-response/v1/find-all`
+  );
+  const fetchMetaQuestion = async() => await axios.get(
+    `${process.env.REACT_APP_HOST}/wp-json/admin/assessment/v1/find-all`
+  );
 
+  const exportType = "xls";
   const export2Excel = (data, fileName, exportType) => {  
-    exportFromJSON({ data, fileName, exportType })  
+    exportFromJSON({ data, fileName, exportType })
   }  
 
-  const exportType = 'xls';
+  useEffect(() => {
+    fetchAllUserResponse().then((res) => {
+      setUserResponse(res.data.data);
+    }).catch(console.error);
+
+    fetchMetaQuestion().then((res) => {
+      setMetaQuestion(res.data.data);
+    }).catch(console.error);
+  }, []);
 
   return (
     <div>
